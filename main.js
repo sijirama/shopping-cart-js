b56a3ae41ee8ec0863ee0f5e35b36af3c9d51191
unknown <gbemilesanmi@gmail.com>
2022-12-26 00:31:17 +0100

    cart count and item button count functionality

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 const shop = document.getElementById('shop')
+const cartamount = document.getElementById('cartamount')
+
 let shopitemdata = [
 {
-    id:'001',
+    id:'1',
     name:'Jean Pants',
     price:45,
     desc:'Jean Pants for a good fit by uniqlo',
     img:'img/corp.jpg'
 },
 {
-    id:'002',
+    id:'2',
     name:'Casual suit jackets',
     price:60,
     desc:'Suit jackets for a good fit by uniqlo',
     img:'img/casgrey.jpg'
 },
 {   
-    id:'003',
+    id:'3',
     name:'Casual T-shirts',
     price:50,
     desc:'Casual T-shirts for a good fit by uniqlo',
     img:'img/casorange.jpg',
 },
 {
-    id:'004',
+    id:'4',
     name:'Angels and Roses T-shirts',
     price:60,
     desc:'Angel and roses merchandise shirts (1999)',
@@ -37,6 +39,7 @@ let shopitemdata = [
 // },
 ]
 
+let basket = []
 
 let generateshop = () => {
     return(shop.innerHTML = shopitemdata.map((x) => {
@@ -49,9 +52,9 @@ let generateshop = () => {
             <div class="price-quant">
                 <h2>$${x.price}</h2>
                 <div class="buttons">
-                    <i class="bi bi-plus-lg"></i>
+                    <i onclick = "increment(${x.id})" class="bi bi-plus-lg"></i>
                     <div id=${x.id} class="quantity">0</div>
-                    <i class="bi bi-dash-lg"></i>
+                    <i onclick = "decrement(${x.id})" class="bi bi-dash-lg"></i>
                 </div>
             </div>
         </div>
@@ -62,3 +65,43 @@ let generateshop = () => {
 }
 
 generateshop()
+
+let increment = (id) => { 
+    let search = basket.find((x)=> x.id === id)
+    if(search === undefined){
+        basket.push(
+            {
+            id:id,
+            item:1
+            }
+        )
+    }else{
+        search.item += 1
+    }
+    // console.log(basket)
+    update()
+    updatebutton(id)
+}
+
+let decrement = (id) => {
+    let search = basket.find((x) => x.id === id)
+    if (search.item > 1){
+        search.item -= 1
+    }else{
+        let indexx = basket.indexOf(search)
+        basket.splice(indexx, 1)
+    }
+    // console.log(basket)
+    update()
+    updatebutton(id)
+}
+
+let update = () => {
+    cartamount.textContent = `${basket.length}`
+}
+
+let updatebutton = (id) =>{
+    const value = document.getElementById(`${id}`)
+    let search = basket.find((x) => x.id === id )
+    value.innerHTML = search.item
+}
